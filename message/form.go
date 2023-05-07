ba669caf1ac342b2ba590f0eebe2a365f5389a05
Aaron Roethe <roethe.aaron@outlook.com>
2023-05-06 17:04:33 -0700

    refactor(message): refactor journal module
    BREAKING CHANGE:

--- message/form.go
+++ message/form.go
@@ -1,150 +1,89 @@
 package message
 
 import (
-	"bytes"
 	"encoding/json"
 	"log"
 	"os"
-	"strings"
-	"text/template"
 
 	survey "github.com/AlecAivazis/survey/v2"
-	"github.com/mitchellh/mapstructure"
-	"github.com/spf13/viper"
 )
 
-type FormItemOption struct {
+type formItemOption struct {
 	Name string
 	Desc string
 }
 
-type FormItem struct {
+type formItem struct {
 	Name     string
 	Desc     string
 	Form     string
-	Options  []*FormItemOption
+	Options  []*formItemOption
 	Required bool
 }
 
-type MessageConfig struct {
-	Items    []*FormItem
+type messageConfig struct {
+	Items    []*formItem
 	Template string
 }
 
-func FillOutForm() ([]byte, error) {
-	// load form
-	if form, tmplText, err := loadForm(); err != nil {
-		log.Printf("loadForm failed, err=%v\n", err)
+func FillOutForm(form []*survey.Question) (map[string]interface{}, error) {
+	answers := map[string]interface{}{}
+	if err := survey.Ask(form, &answers); err != nil {
 		return nil, err
-	} else {
-
-		// ask the question
-		answers := map[string]interface{}{}
-		if err := survey.Ask(form, &answers); err != nil {
-			return nil, err
-		}
-
-		// assemble the answers to commit message
-		var buf bytes.Buffer
-		if err := assembleMessage(&buf, tmplText, answers); err != nil {
-			log.Printf("assemble failed, err=%v\n", err)
-		}
-
-		return buf.Bytes(), nil
 	}
+
+	return answers, nil
 }
 
-func loadForm() (qs []*survey.Question, _ string, err error) {
-	msgConfig := customForm()
+func LoadForm() (qs []*survey.Question, tmplText string, err error) {
+	configFile, err := os.Open("templates/default.json")
+	if err != nil {
+		log.Printf("could not open config file: %v", err)
+		return nil, "", err
+	}
+	defer configFile.Close()
 
-	sub := viper.Sub("message")
-	if sub == nil {
-		log.Printf("no message in config file")
-	} else {
-		if err := sub.Unmarshal(&msgConfig, func(cfg *mapstructure.DecoderConfig) { cfg.ZeroFields = true }); err != nil {
-			log.Printf("ill message in config file, err=%v", err)
-		} else {
-			log.Printf("msg config from file: %v", msgConfig)
-			item := msgConfig.Items[0]
-			log.Printf("msg config item: %s", item.Desc)
-			log.Printf("msg config template: %s", msgConfig.Template)
-		}
+	var msgConfig messageConfig
+	err = json.NewDecoder(configFile).Decode(&msgConfig)
+	if err != nil {
+		log.Printf("could not decode config file: %v", err)
+		return nil, "", err
 	}
 
 	for _, item := range msgConfig.Items {
-		q := survey.Question{
-			Name: item.Name,
-		}
+		q := survey.Question{Name: item.Name}
 		if item.Required {
 			q.Validate = survey.Required
 		}
-		switch item.Form {
-		case "input":
-			q.Prompt = &survey.Input{
-				Message: item.Desc,
-			}
-		case "multiline":
-			q.Prompt = &survey.Multiline{
-				Message: item.Desc,
-			}
-		case "select":
-			prompt := &survey.Select{
-				Message:  item.Desc,
-				PageSize: 8,
-			}
-			for _, option := range item.Options {
-				prompt.Options = append(prompt.Options, option.Desc)
-			}
-			q.Prompt = prompt
-			q.Transform = func(options []*FormItemOption) func(interface{}) interface{} {
-				return func(ans interface{}) (newAns interface{}) {
-					if ans, ok := ans.(survey.OptionAnswer); !ok {
-						return nil
-					} else {
-						ans.Value = options[ans.Index].Name
-						return ans
-					}
-				}
-			}(item.Options)
-		}
+		q.Prompt, q.Transform = CreatePromptAndTransform(item)
 		qs = append(qs, &q)
 	}
 
 	return qs, msgConfig.Template, nil
 }
 
-func customForm() *MessageConfig {
-	// Open the file
-	file, err := os.Open("templates/default.json")
-	if err != nil {
-		return nil
-	}
-	defer file.Close()
-
-	// Read the file contents and unmarshal the JSON data into a variable
-	var config struct{ Message MessageConfig }
-	err = json.NewDecoder(file).Decode(&config)
-	if err != nil {
-		return nil
-	}
-
-	return &config.Message
-}
-
-func assembleMessage(buf *bytes.Buffer, tmplText string, answers map[string]interface{}) error {
-	if tmpl, err := template.New("").Parse(tmplText); err != nil {
-		return err
-	} else {
-		for k, v := range answers {
-			if option, ok := v.(survey.OptionAnswer); ok {
-				answers[k] = option.Value
-			} else if vString, ok := v.(string); ok {
-				answers[k] = strings.TrimSpace(vString)
+func CreatePromptAndTransform(item *formItem) (survey.Prompt, func(interface{}) interface{}) {
+	switch item.Form {
+	case "input":
+		return &survey.Input{Message: item.Desc}, nil
+	case "multiline":
+		return &survey.Multiline{Message: item.Desc}, nil
+	case "select":
+		prompt := &survey.Select{Message: item.Desc, PageSize: 8}
+		for _, option := range item.Options {
+			prompt.Options = append(prompt.Options, option.Desc)
+		}
+		transform := func(options []*formItemOption) func(interface{}) interface{} {
+			return func(ans interface{}) (newAns interface{}) {
+				if ans, ok := ans.(survey.OptionAnswer); ok {
+					ans.Value = options[ans.Index].Name
+					return ans
+				}
+				return nil
 			}
-		}
-		if err := tmpl.Execute(buf, answers); err != nil {
-			return err
-		}
-		return nil
+		}(item.Options)
+		return prompt, transform
+	default:
+		return nil, nil
 	}
 }
--- message/message.go
+++ message/message.go
@@ -2,52 +2,58 @@ package message
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"os"
-	"time"
+	"log"
+	"strings"
+	"text/template"
+
+	survey "github.com/AlecAivazis/survey/v2"
 )
 
-type MessageData struct {
-	Msg       string `json:"msg"`
-	Timestamp int64  `json:"timestamp"`
+func AssembleMessage(buf *bytes.Buffer, tmplText string, answers map[string]interface{}) error {
+	tmpl, err := template.New("").Parse(tmplText)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range answers {
+		switch value := v.(type) {
+		case survey.OptionAnswer:
+			answers[k] = value.Value
+		case string:
+			answers[k] = strings.TrimSpace(value)
+		}
+	}
+
+	return tmpl.Execute(buf, answers)
 }
 
-func SendMessage() error {
-	message, err := FillOutForm()
+func OutputStringTemplate(answers map[string]interface{}, tmplText string) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := AssembleMessage(&buf, tmplText, answers); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func Journal() {
+	form, tmplText, err := LoadForm()
 	if err != nil {
-		return err
+		log.Fatalf("failed to fill load form: %v", err)
 	}
 
-	data := MessageData{
-		Msg:       string(message),
-		Timestamp: time.Now().Unix(),
-	}
-
-	jsonData, _ := json.Marshal(data)
-
-	url := os.Getenv("POST_URL")
-	if url == "" {
-		url = "http://localhost:7071/api/endpoint"
-	}
-
-	req, _ := http.NewRequest(
-		"POST",
-		url,
-		bytes.NewBuffer(jsonData),
-	)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	answers, err := FillOutForm(form)
 	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected response status: %v", resp.StatusCode)
+		log.Fatalf("failed to fill out form: %v", err)
 	}
 
-	return nil
+	fmt.Println("Answers:", answers)
+
+	output, err := OutputStringTemplate(answers, tmplText)
+	if err != nil {
+		log.Fatalf("failed to output string template: %v", err)
+	}
+
+	fmt.Println("Output:", string(output))
 }
